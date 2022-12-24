aa507b9854135a615a61e05c7c1887eaa2d56df0
mualiyu <mualiyuoox@gmail.com>
2022-12-24 09:44:29 +0100

    Update 10

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/agent/login', [App\Http\Controllers\Api\AgentController::class, 'login']);
 Route::post('/agent/update_password', [App\Http\Controllers\Api\AgentController::class, 'update_password']);
 
-Route::get('/device/get_by_mac', [App\Http\Controllers\Api\DeviceController::class, 'show']);
+Route::get('/device/get_by_id', [App\Http\Controllers\Api\DeviceController::class, 'show']);
 Route::get('/device/update_code', [App\Http\Controllers\Api\DeviceController::class, 'update_code']);
 
 Route::post('/transaction/create', [App\Http\Controllers\Api\TransactionController::class, 'create']);
